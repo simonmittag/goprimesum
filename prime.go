c4f8cf1e8f8b61e575f32a2b8db138bc9dc2c473
simonmittag <simonmittag@gmail.com>
2023-07-20 12:11:06 +1000

    initial

--- prime.go
+++ prime.go
@@ -1,18 +1,31 @@
 package goprimesum
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Prime struct{}
 
 func (p *Prime) countTo100() {
-
+	a := make([]int, 100)
+	for i, _ := range a {
+		a[i] = i + 1
+	}
+	for _, ai := range a {
+		p1 := Prime{}
+		if p1.isPrime(ai) {
+			fmt.Printf("\nPrime: %v", ai)
+		}
+	}
 }
 
 func (p *Prime) isPrime(n int) bool {
-	nsq := math.Sqrt(float64(n))
-	if n == 1 {
+	if n <= 1 {
 		return false
 	}
+	nsq := math.Sqrt(float64(n))
+
 	for i := 2; float64(i) <= nsq; i++ {
 		if n%i == 0 {
 			return false
