2f6ea5670536616b004c1ce12064a5d54219f0e1
simonmittag <simonmittag@gmail.com>
2023-07-20 18:43:06 +1000

    go test suite with ginkgo

---
+++ goprimesum_suite_test.go
@@ -0,0 +1,32 @@
+package goprimesum_test
+
+import (
+	"github.com/simonmittag/goprimesum"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGoprimesum(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Goprimesum Suite")
+}
+
+var _ = Describe("PrimeCandidates", func() {
+	var primeCandidate goprimesum.PrimeCandidate
+
+	BeforeEach(func() {
+		primeCandidate = goprimesum.PrimeCandidate{}
+	})
+
+	Describe("Less Than 10", func() {
+		Describe("Special Cases", func() {
+			Context("1", func() {
+				It("is not a prime because it's not divisible by itself AND 1", func() {
+					Expect(primeCandidate.IsPrime(1)).To(BeFalse())
+				})
+			})
+		})
+	})
+})
